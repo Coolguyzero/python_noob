aa29678bb5a8946e1ca0027edd040a4d927cce5f
mrharvest <38403436+Coolguyzero@users.noreply.github.com>
2025-05-25 12:41:39 +0900

    Update LoRAMD.py
    add 3 buttons for advanced options!

--- LoRAMD.py
+++ LoRAMD.py
@@ -5,4 +5,13 @@ root = Tk()
 root.title ("LoRAMD")
 root.geometry("500x600")
 
+btn_test = Button(root,text="function1")
+btn_test.grid()
+
+btn_test = Button(root,text="function2")
+btn_test.grid()
+
+btn_test = Button(root,text="function3")
+btn_test.grid()
+
 root.mainloop()
